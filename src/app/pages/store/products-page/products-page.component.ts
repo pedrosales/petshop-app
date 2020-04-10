300b94ff1b9c07b81ffb634a63f6b44fa1c7fb59
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-10 20:03:12 -0300

    Componentização da listagem de produtos. Buscando os produtos da api.

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html'
 })
 export class ProductsPageComponent implements OnInit {
+  public products$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.products$ = this.data.getProducts();
   }
 
 }
