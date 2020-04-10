26bfe91633fedc1e382139d8ac39aec23932efe6
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-10 20:13:27 -0300

    utilizando o model product

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product.model';
 
 @Injectable({
     providedIn: 'root'
@@ -14,6 +15,6 @@ export class DataService {
     }
 
     getProducts() {
-        return this.http.get<any[]>('http://localhost:3000/v1/products');
+        return this.http.get<Product[]>('http://localhost:3000/v1/products');
     }
 }
