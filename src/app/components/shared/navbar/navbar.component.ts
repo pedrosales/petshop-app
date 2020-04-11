748ad63f00886830b0894a7a2e359894cc791206
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-11 00:27:02 -0300

    Logout

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 import { Security } from 'src/app/utils/security.util';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +10,20 @@ import { Security } from 'src/app/utils/security.util';
 export class NavbarComponent implements OnInit {
   public user: User;
 
+  /**
+   *
+   */
+  constructor(private router: Router) {
+
+  }
+
   ngOnInit(): void {
     this.user = Security.getUser();
+  }
+
+  logout() {
+    Security.clear();
+    this.router.navigate(['/login']);
   }
 
 }
